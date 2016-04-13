11ab27eb073cdd98983ccf5d3cffb2ba022feaaf
pjrcisco <pjrcisco@gmail.com>
2016-04-13 15:03:30 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   post 'api/v1/camera_event' => 'camera_event#handle'
   post 'register_phone'      => 'phone#register'
 
+  post 'api/v1/camera/record'   => 'camera_event#record'
+  post 'api/v1/camera/snapshot' => 'camera_event#snapshot'
+  post 'api/v1/camera/stop'     => 'camera_event#stop'
 
 
   get 'api/v1/camera_event' => 'camera_event#handle_get'
